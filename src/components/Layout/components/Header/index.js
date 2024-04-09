7ece15a3707be97374c6e536261c353481ebcc7a
Le Hoai Nam <lehoainam0403@HoaiNam-Le.local>
2024-04-10 07:42:32 +0900

    Xây Dựng UI Phần Header Dự Án Tiktok #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,34 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
-        <h2 className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
-        </h2>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="TikTok" />
+
+                <div className={cx('search')}>
+                    <input placeholder="Search" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                {/* Actions Button */}
+                <div className={cx('actions')}>Button actions</div>
+            </div>
+        </header>
     );
 }
 
