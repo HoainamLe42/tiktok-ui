4ab6464526d445b5a8b787363d93d6d3309c29b0
Le Hoai Nam <lehoainam0403@HoaiNam-Le.local>
2024-04-23 21:36:06 +0900

    Xây dựng UI phần sidebar #2

---
+++ src/components/SuggestedAccounts/SuggestedAccounts.js
@@ -0,0 +1,25 @@
+import ProsTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './SuggestedAccounts.module.scss';
+import AccountItem from './AccountItem';
+
+const cx = classNames.bind(styles);
+
+function SuggestedAccounts({ label }) {
+    return (
+        <div className={cx('wrapper')}>
+            <p className={cx('label')}>{label}</p>
+            <AccountItem />
+            <AccountItem />
+            <AccountItem />
+
+            <p className={cx('more-btn')}>See more</p>
+        </div>
+    );
+}
+
+SuggestedAccounts.prosTypes = {
+    label: ProsTypes.string.isRequired,
+};
+
+export default SuggestedAccounts;
